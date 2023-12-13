990f54d8e8c5145f5181a8e99133a11dbc14b1ad
Dita Hollmannova <holl.dita@gmail.com>
2023-12-13 22:08:27 +0100

    started day 9

---
+++ 9/9.py
@@ -0,0 +1,18 @@
+from math import lcm
+
+def read_data():
+    f = open("9-full.in", "r")
+    # f = open("9.in", "r")
+    # f = open("9-pt2.in", "r")
+    data = f.read().split("\n")
+
+    return data
+
+
+def solve():
+    pass
+
+
+if __name__ == '__main__':
+    data = read_data()
+    solve(data)
