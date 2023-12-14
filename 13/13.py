ace6ebb285e67c13bbffb3d573ebdd896222f44b
Dita Hollmannova <holl.dita@gmail.com>
2023-12-14 10:04:43 +0100

    solved day 13 pt2

--- 13/13.py
+++ 13/13.py
@@ -19,32 +19,54 @@ def read_data():
     return mirrors
 
 
-def check_reflection(sums, idx):
+def compare(x, y):
+    if x == y:
+        return True, 0
+
+    n = x ^ y  # xor
+    if n & (n-1) == 0:
+        return True, 1
+
+    return False, 0
+
+
+def check_reflection(sums, idx, required_errors=0):
     after = sums[idx:]
     before = sums[:idx]
     before = before[::-1]
+    errors = 0
     # print(list(zip(before, after)))
     for a, b in list(zip(after, before)):
-        if a != b:
+        ok, err = compare(a, b)
+        if not ok:
             return False
+        errors += err
+        if errors > required_errors:
+            return False
+    if errors < required_errors:
+        return False
     return True
 
 
 def solve_mirror(m):
     mirror, rows, cols = m
     for ri in range(len(rows) - 1):
-        if rows[ri] == rows[ri + 1]:
-            if check_reflection(rows, ri + 1):
+        ok, _ = compare(rows[ri], rows[ri + 1])
+        if ok:
+            if check_reflection(rows, ri + 1, required_errors=1):
                 return 100*(ri+1)
     for ci in range(len(cols) - 1):
-        if cols[ci] == cols[ci + 1]:
-            if check_reflection(cols, ci + 1):
+        ok, _ = compare(cols[ci], cols[ci + 1])
+        if ok:
+            if check_reflection(cols, ci + 1, required_errors=1):
                 return ci+1
+    return 0
 
 
 def solve(mirrors):
     summ = 0
     for m in mirrors:
+        # print_mirror(m[0])
         s = solve_mirror(m)
         summ += s
     print(summ)
