682854a3fc8eaf2cc01722c5a00342e0c9b0a29f
Dita Hollmannova <holl.dita@gmail.com>
2023-12-15 19:12:06 +0100

    solved day 15 pt2

--- 15/15.py
+++ 15/15.py
@@ -1,7 +1,3 @@
-import copy
-import hashlib
-
-
 def read_data():
     pt1 = False
     f = open("15-full.in", "r")
@@ -29,6 +25,31 @@ def solve(data):
     return hash_value
 
 
+def solve_v2(data):
+    hashmap = []
+    for i in range(256):
+        hashmap.append({})
+
+    for word in data:
+        if "=" in word:
+            label, focus = word.split("=")
+            box_id = hash(label)
+            hashmap[box_id][label] = focus
+        if "-" in word:
+            label = word.split("-")[0]
+            box_id = hash(label)
+            if label in hashmap[box_id]:
+                hashmap[box_id].pop(label)
+
+    result = 0
+    for box_id in range(256):
+        for i, k in enumerate(hashmap[box_id].keys()):
+            result += (box_id + 1) * (i + 1) * int(hashmap[box_id][k])
+
+    return result
+
+
 if __name__ == '__main__':
     data = read_data()
     print(solve(data))
+    print(solve_v2(data))
