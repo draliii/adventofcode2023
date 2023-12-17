7b241ba915d1aaf113ced34a0798c36b4834a865
Dita Hollmannova <holl.dita@gmail.com>
2023-12-17 10:27:46 +0100

    prepared templates for all days

---
+++ 18/18.py
@@ -0,0 +1,15 @@
+def read_data():
+    pt1 = False
+    f = open("18-full.in", "r")
+    f = open("18.in", "r")
+    data = f.read().split("\n")
+    return data
+
+
+def solve(data):
+    pass
+
+
+if __name__ == '__main__':
+    data = read_data()
+    print(solve(data))
