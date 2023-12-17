9cbd94518342e21f18184ed23533635d2bae805e
Dita Hollmannova <holl.dita@gmail.com>
2023-12-17 12:22:02 +0100

    solved day 17 pt2

--- 17/17.py
+++ 17/17.py
@@ -5,6 +5,7 @@ def read_data():
     pt1 = False
     f = open("17-full.in", "r")
     # f = open("17.in", "r")
+    # f = open("17-v2.in", "r")
     data = f.read().split("\n")
     data = list(map(lambda d: list(map(int, list(d))), data))
     return data
@@ -102,6 +103,94 @@ def solve_v2(data):
 
     pass
 
+
+def get_new_direction_pt2(fromx, fromy, xdif, ydif):
+    # pokud se obraci smer o 180, tak to muzu rovnou zahodit
+    if fromx > 0 and xdif < 0:
+        return False, 0, 0
+    if fromx < 0 and xdif > 0:
+        return False, 0, 0
+    if fromy > 0 and ydif < 0:
+        return False, 0, 0
+    if fromy < 0 and ydif > 0:
+        return False, 0, 0
+
+    newfromx = fromx + xdif
+    newfromy = fromy + ydif
+
+    # pokud se meni smer o 90, musim mit aspon 4
+    if min(abs(newfromx), abs(newfromy)) > 0:
+        if abs(fromx) > 3 or abs(fromy) > 3:
+            return True, xdif, ydif
+        else:
+            return False, 0, 0
+
+    # pokud prelezu 3, tak je to spatne
+    if abs(newfromx) > 10 or abs(newfromy) > 10:
+        return False, 0, 0
+
+    return True, newfromx, newfromy
+
+
+def solve_pt2(data):
+    max_val = sum([sum(d) for d in data])
+
+    options = [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1), (0, 2), (2, 0), (-2, 0), (0, -2),
+               (0, 3), (3, 0), (-3, 0), (0, -3), (0, 4), (4, 0), (-4, 0), (0, -4), (0, 5), (5, 0), (-5, 0), (0, -5),
+               (0, 6), (6, 0), (-6, 0), (0, -6), (0, 7), (7, 0), (-7, 0), (0, -7), (0, 8), (8, 0), (-8, 0), (0, -8),
+               (0, 9), (9, 0), (-9, 0), (0, -9), (0, 10), (10, 0), (-10, 0), (0, -10)]
+
+    end_options = [(0, 4), (4, 0), (-4, 0), (0, -4), (0, 5), (5, 0), (-5, 0), (0, -5),
+               (0, 6), (6, 0), (-6, 0), (0, -6), (0, 7), (7, 0), (-7, 0), (0, -7), (0, 8), (8, 0), (-8, 0), (0, -8),
+               (0, 9), (9, 0), (-9, 0), (0, -9), (0, 10), (10, 0), (-10, 0), (0, -10)]
+
+    options_s = list(map(s, options))
+    end_options_s = list(map(s, end_options))
+
+    best_values = {}
+    for o in options_s:
+        best_values[o] = new_array(len(data), len(data[0]), initval=max_val)
+        best_values[o][0][0] = 0
+
+    queue = PriorityQueue()
+    # value, x, y, fromx, fromy
+    queue.put((0, 0, 0, 0, 0))
+
+    while True:
+        if queue.empty():
+            break
+
+        value, x, y, fromx, fromy = queue.get()
+
+        for xdif, ydif in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            newx = x + xdif
+            newy = y + ydif
+
+            success, newfromx, newfromy = get_new_direction_pt2(fromx, fromy, xdif, ydif)
+            if not success:
+                continue
+
+            # do not leave the array
+            if newx >= len(data) or newx < 0 or newy >= len(data[0]) or newy < 0:
+                continue
+
+            prev_val = best_values[s((newfromx, newfromy))][newx][newy]
+            loss = data[newx][newy]
+            new_val = value + loss
+
+            if new_val < prev_val:
+                best_values[s((newfromx, newfromy))][newx][newy] = new_val
+                queue.put((new_val, newx, newy, newfromx, newfromy))
+
+    min_value = max_val
+    for o in end_options_s:
+        # print(o, best_values[o][len(data)-1][len(data[0])-1])
+        if best_values[o][len(data)-1][len(data[0])-1] < min_value:
+            min_value = best_values[o][len(data)-1][len(data[0])-1]
+    print(min_value)
+
+    pass
+
 if __name__ == '__main__':
     data = read_data()
-    print(solve_v2(data))
+    print(solve_pt2(data))
