13bd91469b5adf1d54b90e0f01d30d35d69b90b4
Dita Hollmannova <holl.dita@gmail.com>
2024-01-09 19:03:29 +0100

    Renaming mistake in day 21

--- 21/21.py
+++ 21/21.py
@@ -1,8 +1,8 @@
 def read_data():
     pt1 = False
-    f = open("20-full.in", "r")
-    f = open("20-triple-full.in", "r")  # 3x3 grid
-    #f = open("20.in", "r")
+    f = open("21-full.in", "r")
+    f = open("21-triple-full.in", "r")  # 3x3 grid
+    #f = open("21.in", "r")
     data = f.read().split("\n")
     stones = new_grid(len(data[0]), len(data), 0)
     positions = new_grid(len(data[0]), len(data), 0)
